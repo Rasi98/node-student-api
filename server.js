d631139cdc5568b0cf665137923fd16a69291796
lakshan.rasingolla <lakshan.rasingolla@cadissoftware.com>
2023-10-09 21:59:21 +0530

    MongoDB connection configured

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config'
+
 const app = express();
 
-const port = 3000;
-
-//routes
 app.get('/', (req,res) => {
     res.send('Hello worlds!')
 })
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+//connet to MongoDB Atlas
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log('Connected to MongoDB');
+    //start local server (listening)
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is listening on port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log(error);
 })
