76f54822b99e78c37b456f5c4c265f52989e4b11
lakshan.rasingolla <lakshan.rasingolla@cadissoftware.com>
2023-10-20 21:51:45 +0530

    cors enabled

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import "dotenv/config";
+
 import studentRouter from "./routes/studentRoute.js";
 import errorMiddleware from "./middleware/errorMiddleware.js";
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -10,6 +13,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use("/api/students", studentRouter);
 
